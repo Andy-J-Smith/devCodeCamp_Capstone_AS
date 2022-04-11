37a0ce341d0d0ad04481667dfaaa64fb4c8b96db
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-04-11 17:01:23 -0500

    adjusted product css

--- frontend/src/components/Product/Product.jsx
+++ frontend/src/components/Product/Product.jsx
@@ -8,8 +8,8 @@ const Product = (props) => {
 
     const navigate = useNavigate();
   return (
-    <div className="container">
-      <Card style={{ width: "18rem" }}>
+    <div className="product-container">
+      <Card className="good" style={{ width: "18rem" }}>
         <Card.Img variant="top" src="/tacklebox.jpeg" />
         <Card.Body>
           <Card.Title><h2>Perch Package</h2></Card.Title>
@@ -18,11 +18,12 @@ const Product = (props) => {
             <li>Sinkers</li>
             <li>Bobbers</li>
             <li>Fishing Line</li>
+            <p>$25.00</p>
           </Card.Text>
           <Button variant="primary" onClick={() => navigate("/checkout")}>Check Out</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: "18rem" }}>
+      <Card className="best" style={{ width: "18rem" }}>
         <Card.Img variant="top" src="/tacklebox.jpeg" />
         <Card.Body>
           <Card.Title><h2>Slab Slayer Package</h2></Card.Title>
@@ -36,11 +37,12 @@ const Product = (props) => {
             <li>Spider rig asscessories</li>
             <li>Scent</li>
             <li>Jig Bodies</li>
+            <p>$45.00</p>
           </Card.Text>
           <Button variant="primary" onClick={() => navigate("/checkout")}>Check Out</Button>
         </Card.Body>
       </Card>
-      <Card style={{ width: "18rem" }}>
+      <Card className="better" style={{ width: "18rem" }}>
         <Card.Img variant="top" src="/tacklebox.jpeg" />
         <Card.Body>
           <Card.Title><h2>Sac-a-Lait Package</h2></Card.Title>
@@ -51,6 +53,7 @@ const Product = (props) => {
             <li>Fishing Line</li>
             <li>Jig heads</li>
             <li>Crankbaits</li>
+            <p>$35.00</p>
           </Card.Text>
           <Button variant="primary" onClick={() => navigate("/checkout")}>Check Out</Button>
         </Card.Body>
