293317da96efd17d071f5e83419ca9484dcf9654
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-04-11 15:11:42 -0500

    created user survey form + tested ok

--- frontend/src/components/Survey/Survey.jsx
+++ frontend/src/components/Survey/Survey.jsx
@@ -1,40 +1,94 @@
-import axios from 'axios'
-import React, {useState, useEffect} from 'react'
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import useAuth from "../../hooks/useAuth";
+import "./Survey.css";
 
 const Survey = (props) => {
-  const [newSurvey, setNewSurvey] = useState('')
-  const [survey, setSurvey] = useState('')
-
-  useEffect(() => {
-    getAllSurveys();
-  }, []);
-
-  async function getAllSurveys(){
-      let response = await axios.get("http://127.0.0.1:8000/api/surveys/");
-      setSurvey(response.data);
-      console.log(response.data)
-
-  }
- 
-  
+  const [species, setSpecies] = useState("");
+  const [style, setStyle] = useState("");
+  const [frequency, setFrequency] = useState("");
+  const [water_type, setWaterType] = useState("");
+  const [boat_land, setBoatLand] = useState("");
+  const [survey, setSurvey] = useState("");
+  const [user, token] = useAuth();
 
   async function createSurvey(newSurvey) {
-    let response = await axios.post(
-      "http://127.0.0.1:8000/api/surveys/",
-      newSurvey
-    );
-    if (response.status === 201) {
-      await getAllSurveys();
+    try {
+      let response = await axios.post(
+        "http://127.0.0.1:8000/api/surveys/new/",
+        newSurvey,
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        }
+      );
+      setSurvey(newSurvey);
+    } catch (error) {
+      console.log(error.message);
     }
   }
 
-  
-  
-  
-    return (
-    <div>Survey</div>
-  )
-}
+  function handleSurvey(event) {
+    event.preventDefault();
+    let newSurvey = {
+      species: species,
+      style: style,
+      frequency: frequency,
+      water_type: water_type,
+      boat_land: boat_land,
+    };
+    console.log(newSurvey);
+    createSurvey(newSurvey);
+  }
 
-export default Survey
+  return (
+    <div>
+      <div className="container">
+        Please fill out the below Survey
+        <form className="form" onSubmit={handleSurvey}>
+          <label>Tell us what type of fish you target:</label>
+          <input
+            type="text"
+            placeholder="species"
+            value={handleSurvey.species}
+            onChange={(event) => setSpecies(event.target.value)}
+          />
+          <label>Input type of fishing, live bait or artificial:</label>
+          <input
+            type="text"
+            placeholder="style"
+            value={handleSurvey.style}
+            onChange={(event) => setStyle(event.target.value)}
+          />
+          <label>How often do you fish? weekly/monthly:</label>
+          <input
+            type="text"
+            placeholder="frequency"
+            value={handleSurvey.frequency}
+            onChange={(event) => setFrequency(event.target.value)}
+          />
+          <label>
+            Do you fish rivers, lakes, ocean, ponds, creeks, or other:
+          </label>
+          <input
+            type="text"
+            placeholder="water_type"
+            value={handleSurvey.water_type}
+            onChange={(event) => setWaterType(event.target.value)}
+          />
+          <label>Tell us how you fish, boat or from land:</label>
+          <input
+            type="text"
+            placeholder="boat_land"
+            value={handleSurvey.boat_land}
+            onChange={(event) => setBoatLand(event.target.value)}
+          />
+          <button type="submit">Submit Survey</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Survey;
--- frontend/src/pages/AdminPage/AdminPage.jsx
+++ frontend/src/pages/AdminPage/AdminPage.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
-import Survey from '../../components/Survey/Survey'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import { renderMatches } from 'react-router-dom';
+import Survey from '../../components/Survey/Survey';
+
 
 const AdminPage = (props) => {
+
+  const [survey, setSurvey] = useState('')
+
+  useEffect(() => {
+    getAllSurveys();
+  }, []);
+
+  async function getAllSurveys(){
+      let response = await axios.get("http://127.0.0.1:8000/api/surveys/");
+      setSurvey(response.data);
+      console.log(response.data)
+
+  }
+
   return (
     <div>
         <p>Admin Page</p>
-        <Survey/>
-
+        <table className="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Boat or land</th>
+          <th>Frequency</th>
+          <th>Species</th>
+          <th>Style</th>
+          <th>Water Type</th>
+        </tr>
+      </thead>
+      <tbody>
+        {survey && survey.map((survey, index) => {
+          return (
+            <tr key={index}>
+              <td>{index +1}</td>
+              <td>{survey.boat_land}</td>
+              <td>{survey.frequency}</td>
+              <td>{survey.species}</td>
+              <td>{survey.style}</td>
+              <td>{survey.water_type}</td>
+             
+       
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+       
 
     </div>
   )
