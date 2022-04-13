6b20d9f4728a7fe5780d8cc3c11fc4a6afaaafcd
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-04-13 18:20:07 -0500

    landing page styling

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -9,14 +9,13 @@ const LandingPage = (props) => {
   return (
     <div className="hero">
       
-      <Card className="hero-card" style={{ width: "30rem" }}>
-        <Card.Img variant="top" src={poster} />
+      <Card className="hero-card" style={{ width: "50rem", height: "65rem" }}>
+        <Card.Img className='hero-card-img'variant="top" src={poster} />
         <Card.Body>
           <Card.Title className="hero-title">Fishing Tackle Subscription Service</Card.Title>
           <Card.Text className="hero-card-txt">
             Where Your Fishing Adventure Begins!
           </Card.Text>
-          <Button variant="primary">Video</Button>
         </Card.Body>
         <Survey/>
       </Card>
