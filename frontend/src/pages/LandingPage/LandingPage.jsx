7b5326af69575a8eef616c53fb1579553dff109a
Andy-J-Smith <98605897+Andy-J-Smith@users.noreply.github.com>
2022-04-13 17:26:37 -0500

    landing page background

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import './LandingPage.css'
-import Survey from '../../components/Survey/Survey'
+import React from "react";
+import "./LandingPage.css";
+import Survey from "../../components/Survey/Survey";
+import poster from '../../Assets/posters.jpg';
+import Card from 'react-bootstrap/Card'
+import Button from "react-bootstrap/esm/Button";
 
 const LandingPage = (props) => {
   return (
-    <div className='hero'>
-      <Survey/>
-
-        
+    <div className="hero">
+      
+      <Card className="hero-card" style={{ width: "30rem" }}>
+        <Card.Img variant="top" src={poster} />
+        <Card.Body>
+          <Card.Title className="hero-title">Fishing Tackle Subscription Service</Card.Title>
+          <Card.Text className="hero-card-txt">
+            Where Your Fishing Adventure Begins!
+          </Card.Text>
+          <Button variant="primary">Video</Button>
+        </Card.Body>
+        <Survey/>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
